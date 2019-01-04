58520e9da7ba2b65532ada0c48a590e1ac0eed37
Paul Whittemore <dev@appurist.com>
2019-01-04 00:36:31 -0400

    A quick test of the Javalin example and a pom.xml

---
+++ src/main/java/com/appurist/JavalinTest.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class JavalinTest {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
