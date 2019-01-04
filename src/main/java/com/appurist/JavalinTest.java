dae1f6ffdf4abac3c525f7653b9907d2a53cdbd0
Paul Whittemore <dev@appurist.com>
2019-01-04 11:16:27 -0400

    Added request logging and timing, documented static and web jars.

--- src/main/java/com/appurist/JavalinTest.java
+++ src/main/java/com/appurist/JavalinTest.java
@@ -4,7 +4,12 @@ import io.javalin.Javalin;
 
 public class JavalinTest {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+        Javalin app = Javalin.create();
+
+        app.requestLogger((ctx, timeMs) -> {
+            // prints "GET /hello took 4.5 ms"
+            System.out.println(ctx.method() + " "  + ctx.path() + " took " + timeMs + " ms");
+        });
 
         app.before("/some-path/*", ctx -> {
             // runs before all request to /some-path/*
@@ -12,6 +17,7 @@ public class JavalinTest {
         app.before(ctx -> {
             // calls before("/*", handler)
         });
+
 
         /* Straight from the example page
             at https://javalin.io/documentation#handler-groups
@@ -35,6 +41,15 @@ public class JavalinTest {
         app.get("/hello/*/and/*", ctx -> {
             ctx.result("Hello: " + ctx.splat(0) + " and " + ctx.splat(1));
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> ctx.result("Hello World"));
+
+        // If enabled, {classpath}/static/index.html will be available via http://{host}:{port}/index.html
+        // app.get("/" ...) above would override gets to /
+        // app.enableStaticFiles("/static");
+
+        // if enabled, available at /webjars/name/version/file.ext
+        // app.enableWebJars();
+
+        app.start(7000);
     }
 }
